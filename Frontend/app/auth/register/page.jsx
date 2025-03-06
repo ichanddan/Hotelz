f2a254d292c83f532795770dee4ce9fd0b1f30a3
chandan083 <code.us.cm@gmail.com>
2025-03-06 21:08:10 +0530

    refactor registration page: reorganize imports and improve code structure

--- Frontend/app/auth/register/page.jsx
+++ Frontend/app/auth/register/page.jsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { register } from "@/Api";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Card,
   CardContent,
@@ -14,9 +10,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toast } from "sonner";
-import { register } from "@/Api";
-// import {register} from '../../../Api/index'
+
 export default function Register() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
