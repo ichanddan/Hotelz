fca4f587d414ccec87ebc39d686ebb51081fc5f0
chandan083 <code.us.cm@gmail.com>
2025-03-06 21:07:33 +0530

    add CORS middleware to backend and update package.json dependencies

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { router } = require("./routes");
+const cors = require('cors')
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors())
 app.use("/api", router);
 
 app.listen(port, () => {
