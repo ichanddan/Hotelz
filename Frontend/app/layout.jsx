2aee12b32ae6d6443d6b552e55732c112eef441a
chandan083 <code.us.cm@gmail.com>
2025-03-07 12:25:37 +0530

    add toast notifications: integrate Toaster component for user feedback

--- Frontend/app/layout.jsx
+++ Frontend/app/layout.jsx
@@ -1,9 +1,12 @@
+import { Toaster } from "sonner";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Toaster position="top-right" richColors />
+      </body>
     </html>
   );
 }
