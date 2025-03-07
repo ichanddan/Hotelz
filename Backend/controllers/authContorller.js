4b64ebe39c0a00583c0d7d0015fed4901303ddc3
chandan083 <code.us.cm@gmail.com>
2025-03-07 11:15:58 +0530

    refactor registration logic: add email uniqueness check, hash passwords, and improve error handling

--- Backend/controllers/authContorller.js
+++ Backend/controllers/authContorller.js
@@ -1,28 +1,41 @@
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
+const bcrypt = require("bcryptjs");
 
-const register = (req, res) => {
+const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    if (!name) return res.status(400).json({ message: "Name is require" });
-    if (!email) return res.status(400).json({ message: "Email is require" });
-    if (!password)
-      return res.status(400).json({ message: "Password is require" });
+    console.log(req.body)
 
-    const register = prisma.user.create({ name, email, password });
-    if (register) {
+    if (!name) return res.status(400).json({ message: "Name is required" });
+    if (!email) return res.status(400).json({ message: "Email is required" });
+    if (!password)
+      return res.status(400).json({ message: "Password is required" });
+
+    const findEmail = await prisma.user.findUnique({ where: { email } });
+    if (findEmail) {
+      return res.status(409).json({ message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+
+    if (newUser) {
       return res
-        .status(200)
+        .status(201)
         .json({ message: "Register Success", success: true });
     }
     return res.status(400).json({ message: "Register failed", success: false });
   } catch (error) {
-    res.status(500).json({ message: "internal server error", error });
+    console.error(error);
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
 
-
-module.exports ={
-    register
-}
+module.exports = {
+  register,
+};
